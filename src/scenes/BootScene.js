3d9599dd62eac8f8af22283f999a491a888192c3
behzodasadullaev <bekasad.work@gmail.com>
2026-05-23 12:40:44 +0500

    feat: generate tank, cover, pickup, and shield-ring textures

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-import { COLORS, PLAYER, ENEMY, BULLET } from '../config/constants.js';
+import { COLORS, PLAYER, ENEMY, BULLET, TANK, COVER, POWERUP } from '../config/constants.js';
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
@@ -29,6 +29,126 @@ export default class BootScene extends Phaser.Scene {
     this.makeParticleTexture();
     this.makeMuzzleFlashTexture();
     this.makeFloorTileTexture();
+    this.makeTankHullTexture('tank-hull-player', COLORS.player, 0x0f2a2a);
+    this.makeTankHullTexture('tank-hull-enemy', COLORS.enemy, 0x331017);
+    this.makeTankTurretTexture('tank-turret-player', COLORS.player);
+    this.makeTankTurretTexture('tank-turret-enemy', COLORS.enemy);
+    this.makeCoverBlockTexture();
+    this.makePickupTextures();
+    this.makeShieldRingTexture();
+    this.makeSparkTexture();
+  }
+
+  makeTankHullTexture(key, fill, dark) {
+    const w = TANK.hullW;
+    const h = TANK.hullH + 8;
+    const g = this.add.graphics();
+    g.fillStyle(dark, 1);
+    g.fillRect(0, 0, w, 6);
+    g.fillRect(0, h - 6, w, 6);
+    g.fillStyle(0x000000, 0.4);
+    for (let x = 2; x < w - 2; x += 5) {
+      g.fillRect(x, 1, 2, 4);
+      g.fillRect(x, h - 5, 2, 4);
+    }
+    g.fillStyle(fill, 1);
+    g.fillRoundedRect(2, 6, w - 4, h - 12, 3);
+    g.lineStyle(1, 0xffffff, 0.5);
+    g.strokeRoundedRect(2, 6, w - 4, h - 12, 3);
+    g.generateTexture(key, w, h);
+    g.destroy();
+  }
+
+  makeTankTurretTexture(key, fill) {
+    const w = TANK.turretW + TANK.barrelLen + 4;
+    const h = TANK.turretH;
+    const g = this.add.graphics();
+    g.fillStyle(fill, 1);
+    g.fillCircle(TANK.turretW / 2 + 2, h / 2, TANK.turretW / 2 + 1);
+    g.lineStyle(1, 0xffffff, 0.6);
+    g.strokeCircle(TANK.turretW / 2 + 2, h / 2, TANK.turretW / 2 + 1);
+    g.fillStyle(0xffffff, 1);
+    g.fillRect(TANK.turretW + 2, h / 2 - 2, TANK.barrelLen, 4);
+    g.generateTexture(key, w, h);
+    g.destroy();
+  }
+
+  makeCoverBlockTexture() {
+    const w = 32;
+    const h = 32;
+    const g = this.add.graphics();
+    g.fillStyle(COVER.fill, 1);
+    g.fillRoundedRect(0, 0, w, h, 4);
+    g.lineStyle(2, COVER.edge, COVER.edgeAlpha);
+    g.strokeRoundedRect(1, 1, w - 2, h - 2, 4);
+    g.fillStyle(0x000000, 0.3);
+    g.fillCircle(5, 5, 1.5);
+    g.fillCircle(w - 5, 5, 1.5);
+    g.fillCircle(5, h - 5, 1.5);
+    g.fillCircle(w - 5, h - 5, 1.5);
+    g.generateTexture('cover-block', w, h);
+    g.destroy();
+  }
+
+  makePickupTextures() {
+    const size = POWERUP.iconSize;
+
+    let g = this.add.graphics();
+    g.fillStyle(0x002a14, 1);
+    g.fillCircle(size / 2, size / 2, size / 2);
+    g.fillStyle(0x4dff88, 1);
+    g.fillRect(size / 2 - 8, size / 2 - 2, 16, 4);
+    g.fillRect(size / 2 - 2, size / 2 - 8, 4, 16);
+    g.lineStyle(1, 0xffffff, 0.8);
+    g.strokeCircle(size / 2, size / 2, size / 2 - 1);
+    g.generateTexture('pickup-health', size, size);
+    g.destroy();
+
+    g = this.add.graphics();
+    g.fillStyle(0x331a00, 1);
+    g.fillCircle(size / 2, size / 2, size / 2);
+    g.lineStyle(2, 0xffa040, 1);
+    for (let i = 0; i < 3; i++) {
+      const dx = -6 + i * 6;
+      g.beginPath();
+      g.moveTo(size / 2 - 5 + dx, size / 2 + 4);
+      g.lineTo(size / 2 + dx, size / 2 - 4);
+      g.lineTo(size / 2 + 5 + dx, size / 2 + 4);
+      g.strokePath();
+    }
+    g.lineStyle(1, 0xffffff, 0.8);
+    g.strokeCircle(size / 2, size / 2, size / 2 - 1);
+    g.generateTexture('pickup-triple', size, size);
+    g.destroy();
+
+    g = this.add.graphics();
+    g.fillStyle(0x001a33, 1);
+    g.fillCircle(size / 2, size / 2, size / 2);
+    g.lineStyle(3, 0x4da6ff, 1);
+    g.strokeCircle(size / 2, size / 2, size / 2 - 4);
+    g.lineStyle(1, 0xffffff, 0.9);
+    g.strokeCircle(size / 2, size / 2, size / 2 - 1);
+    g.generateTexture('pickup-shield', size, size);
+    g.destroy();
+  }
+
+  makeShieldRingTexture() {
+    const size = PLAYER.radius * 4;
+    const g = this.add.graphics();
+    g.lineStyle(3, 0x4da6ff, 0.9);
+    g.strokeCircle(size / 2, size / 2, size / 2 - 2);
+    g.lineStyle(1, 0xffffff, 0.6);
+    g.strokeCircle(size / 2, size / 2, size / 2 - 5);
+    g.generateTexture('shield-ring', size, size);
+    g.destroy();
+  }
+
+  makeSparkTexture() {
+    const g = this.add.graphics();
+    g.fillStyle(0x00ffe0, 1);
+    g.fillCircle(3, 3, 3);
+    g.generateTexture('spark', 6, 6);
+    g.destroy();
   }
 
   makePlayerTexture() {
