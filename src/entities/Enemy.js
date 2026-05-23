c58e8d9c121f6f06506f29f8add26cb153eebf95
behzodasadullaev <bekasad.work@gmail.com>
2026-05-23 12:41:56 +0500

    feat: convert enemies to tank sprites with tracking turret

--- src/entities/Enemy.js
+++ src/entities/Enemy.js
@@ -1,12 +1,17 @@
 import Phaser from 'phaser';
-import { ENEMY } from '../config/constants.js';
+import { ENEMY, TANK } from '../config/constants.js';
 
 export default class Enemy extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
-    super(scene, x, y, 'enemy');
+    super(scene, x, y, 'tank-hull-enemy');
     scene.add.existing(this);
     scene.physics.add.existing(this);
-    this.body.setCircle(ENEMY.radius, this.width / 2 - ENEMY.radius, this.height / 2 - ENEMY.radius);
+
+    const bodyR = Math.min(TANK.hullW, TANK.hullH) / 2;
+    this.body.setCircle(bodyR, this.width / 2 - bodyR, this.height / 2 - bodyR);
+
+    this.turret = scene.add.image(x, y, 'tank-turret-enemy').setDepth(this.depth + 1);
+
     this.hp = ENEMY.baseHp;
     this.maxHp = ENEMY.baseHp;
     this.speed = ENEMY.baseSpeed;
@@ -22,27 +27,39 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
   update(time, delta, target) {
     if (!this.alive || !target || !target.alive) {
       this.body.setVelocity(0, 0);
+      this.turret.setPosition(this.x, this.y);
       return;
     }
     const angle = Phaser.Math.Angle.Between(this.x, this.y, target.x, target.y);
-    this.rotation = angle + Math.PI / 2; // face the player (eyes up)
+    this.rotation = angle;
     this.body.setVelocity(Math.cos(angle) * this.speed, Math.sin(angle) * this.speed);
+    this.turret.setPosition(this.x, this.y).setRotation(angle);
+    this.turret.setVisible(this.visible);
   }
 
   takeDamage(amount) {
     if (!this.alive) return false;
     this.hp -= amount;
-    // Hit flash
     this.scene.tweens.killTweensOf(this);
     this.setTint(0xffffff);
+    if (this.turret) this.turret.setTint(0xffffff);
     this.scene.time.delayedCall(70, () => {
-      if (this.active) this.clearTint();
+      if (this.active) {
+        this.clearTint();
+        if (this.turret) this.turret.clearTint();
+      }
     });
     return this.hp <= 0;
   }
 
   die() {
     this.alive = false;
+    if (this.turret) this.turret.setVisible(false);
     this.disableBody(true, true);
+  }
+
+  destroy(fromScene) {
+    if (this.turret) this.turret.destroy();
+    super.destroy(fromScene);
   }
 }
